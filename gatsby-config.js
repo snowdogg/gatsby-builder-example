e1047f59a3e61ba8f13b59e52c0c58b44e076ee1
Builder Work Computer <andreascary@BuilderOAndreas.Starry-inc.net>
2023-10-19 16:30:59 -0700

    basic functionality working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Builder Example`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-typescript',
+    {
+      resolve: '@builder.io/gatsby',
+      options: {
+        /** TODO: update this with your API key! */
+        publicAPIKey: 'jdGaMusrVpYgdcAnAtgn',
+        useCache: false,
+        // to allow editing on local host
+        custom404Dev: path.resolve('src/pages/404.jsx'),
+        templates: {
+          // Render every `page` model as a new page using the /page.tsx template
+          // based on the URL provided in Builder.io
+          page: path.resolve('src/templates/page.jsx')
+        }
+      }
+    }
+  ]
 }
---
+++ src/templates/page.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { BuilderComponent, builder } from '@builder.io/react';
+import { Helmet } from 'react-helmet';
+import '@builder.io/widgets';
+/**
+ * Hero is an example of a custom component that you can use in the builder.io editor
+ * https://www.builder.io/c/docs/custom-react-components
+ */
+// import '../components/Hero/Hero.builder';
+
+// TODO: enter your public API key
+builder.init('jdGaMusrVpYgdcAnAtgn');
+
+const PageTemplate = ({ data }) => {
+  const content = data.allBuilderModels.page[0]?.content;
+  return (
+    <>
+      <Helmet>
+        <title>{content?.data.title}</title>
+      </Helmet>
+      <header>
+        <h1>Gatsby Minimal Starter</h1>
+      </header>
+
+      <BuilderComponent content={content} model="page" />
+      <footer>
+        <p>A Builder.io starter with Gatsby</p>
+      </footer>
+    </>
+  );
+};
+
+export default PageTemplate;
+export const pageQuery = graphql`
+  query ($path: String!) {
+    allBuilderModels {
+      page(target: { urlPath: $path }, limit: 1, options: { cachebust: true }) {
+        content
+      }
+    }
+  }
+`;
